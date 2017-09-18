d7f74318cbc9f45829005ab28e3af030d64605d7
Shayevitz <idoideasmail@gmail.com>
2017-09-18 04:05:25 +0300

    Android O crashing fixed

--- app/src/main/java/com/idoideas/xoutof10/OverlayService.java
+++ app/src/main/java/com/idoideas/xoutof10/OverlayService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Gravity;
@@ -16,6 +17,7 @@ import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static android.view.WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+import static android.view.WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
 import static android.view.WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
 
 /**
@@ -40,7 +42,7 @@ public class OverlayService extends Service {
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 100,
                 // Allows the view to be on top of the StatusBar
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                Build.VERSION.SDK_INT < 26 ? WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY : TYPE_APPLICATION_OVERLAY,
                 // Keeps the button presses from going to the background window
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                         // Enables the notification to recieve touch events
@@ -60,7 +62,7 @@ public class OverlayService extends Service {
                 100,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 // Allows the view to be on top of the StatusBar
-                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                Build.VERSION.SDK_INT < 26 ? WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY : TYPE_APPLICATION_OVERLAY,
                 // Keeps the button presses from going to the background window
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
